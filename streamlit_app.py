f5727f14f342cb9b6c7f905078a426f4cf09bd60
Shambhu-Ak <137255161+Shambhu-Ak@users.noreply.github.com>
2023-07-20 10:21:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("WELCOME TO CAPGEMINI")
+streamlit.header("BREAKFAST MENU")
+streamlit.text("Omega 3 &blueberry oatmel")
+streamlit.text("Hard-boiled free-range egg")
+
